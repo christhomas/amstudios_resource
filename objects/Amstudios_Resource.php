28f45daf160e4c308f0cc7c5788e03316bec16fa
Chris Thomas <christopher.t@mobfox.com>
2015-09-14 16:11:54 +0200

    - start to rebuild this code as a separate module so I can use it in other contexts

---
+++ objects/Amstudios_Resource.php
@@ -0,0 +1,169 @@
+<?php
+/*******************************************************************************
+ * Copyright (c) {15/03/2008} {Christopher Thomas}
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Contributors/Author:
+ *    {Christopher Thomas} - Creator - chris.thomas@antimatter-studios.com
+ *
+ *******************************************************************************/
+
+/**
+ * 	class:	Amstudios_Resource
+ *
+ *	description: todo, write description
+ *
+ * 	todo: write documentation
+ */
+class Amstudios_Resource extends Amslib_MVC
+{
+    static protected $stylesheet = array("__common"=>array());
+    static protected $javascript = array("__common"=>array());
+    static protected $default_position = "__common";
+
+    static protected function validatePosition($position=NULL)
+    {
+        return is_string($position) && strlen($position)
+            ? $position
+            : self::$default_position;
+    }
+
+    /**
+     * 	method:	addStylesheet
+     *
+     * 	todo: write documentation
+     */
+    static public function addStylesheet($id,$file,$conditional=NULL,$media=NULL,$position=NULL)
+    {
+        $position = self::validatePosition($position);
+
+        if(!isset(self::$stylesheet[$position])) self::$stylesheet[$position] = array();
+        $p = &self::$stylesheet[$position];
+
+        if($id && $file){
+            $media = $media ? "media='$media'" : "";
+            $p[$id] = "<link rel='stylesheet' type='text/css' href='$file' $media />";
+
+            if(is_string($conditional) && strlen($conditional)){
+                $p[$id] = "<!--[$conditional]>{$p[$id]}<![endif]-->";
+            }
+        }
+
+        unset($p); // release the reference (prevents problems with loops)
+    }
+
+    /**
+     * 	method:	getStylesheet
+     *
+     * 	todo: write documentation
+     */
+    static public function getStylesheet($position=NULL)
+    {
+        $position = self::validatePosition($position);
+
+        if(isset(self::$stylesheet[$position])){
+            return implode("\n",self::$stylesheet[$position]);
+        }
+
+        Amslib_Debug::log(
+            "the requested position to obtain the stylesheets from was not valid",
+            $position,
+            array_keys(self::$stylesheet)
+        );
+
+        return "";
+    }
+
+    /**
+     * 	method:	removeStylesheet
+     *
+     * 	todo: write documentation
+     */
+    static public function removeStylesheet($id,$position=NULL)
+    {
+        $position = self::validatePosition($position);
+
+        if(!isset(self::$stylesheet[$position])){
+            Amslib_Debug::log("position requested to remove stylesheet from is not exist");
+        }else if(!isset(self::$stylesheet[$position][$id])){
+            Amslib_Debug::log("index requested to remove from stylesheet array is not valid");
+        }else{
+            unset(self::$stylesheet[$position][$id]);
+        }
+    }
+
+    /**
+     * 	method:	addJavascript
+     *
+     * 	todo: write documentation
+     */
+    static public function addJavascript($id,$file,$conditional=NULL,$position=NULL)
+    {
+        $position = self::validatePosition($position);
+
+        if(!isset(self::$javascript[$position])) self::$javascript[$position] = array();
+        $p = &self::$javascript[$position];
+
+        if($id && $file){
+            $p[$id] = "<script type='text/javascript' src='$file'></script>";
+
+            if(is_string($conditional) && strlen($conditional)){
+                $p[$id] = "<!--[$conditional]>{$p[$id]}<![endif]-->";
+            }
+        }
+
+        unset($p); // release the reference (prevents problems with loops)
+    }
+
+    /**
+     * 	method:	getJavascript
+     *
+     * 	todo: write documentation
+     */
+    static public function getJavascript($position=NULL)
+    {
+        $position = self::validatePosition($position);
+
+        if(isset(self::$javascript[$position])){
+            return implode("\n",self::$javascript[$position]);
+        }
+
+        Amslib_Debug::log(
+            "the requested position to obtain the javascripts from was not valid",
+            $position,
+            array_keys(self::$javascript)
+        );
+
+        return "";
+    }
+
+    /**
+     * 	method:	removeJavascript
+     *
+     * 	todo: write documentation
+     */
+    static public function removeJavascript($id,$position=NULL)
+    {
+        $position = self::validatePosition($position);
+
+        if(!isset(self::$javascript[$position])){
+            Amslib_Debug::log("position requested to remove javascript from is not valid");
+        }else if(!isset(self::$javascript[$position][$id])){
+            Amslib_Debug::log("index requested to remove from javascript array is not valid");
+        }else{
+            unset(self::$javascript[$position][$id]);
+        }
+    }
+}
